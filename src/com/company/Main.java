6a68b10fd12c728b16ee18862b0026361e5452ac
DrPhannibal <mahdisouri0@gmail.com>
2022-04-19 23:06:43 +0430

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        double number_1=6,number_2=3,number_3=0;
+//        System.out.println(number_1+number_2);
+//        System.out.println(number_1-number_2);
+//        System.out.println(number_1*number_2);
+//        System.out.println(number_1/number_2);
+//        System.out.println(number_2-number_1);
+//        System.out.println(number_2/number_1);
+
+//        number_1=number_3;
+//
+//        System.out.println(number_1);
+
+//        System.out.println(number_1+number_2*(number_1/number_2));
+
+//        int a=9;
+//        int b=4;
+////        System.out.println(a/b);
+//
+//        if (a==b){
+//            System.out.println("==");
+//        }
+//
+//        if (a!=b){
+//            System.out.println("!=");
+//        }
+//
+//        if (a>b){
+//            System.out.println(">");
+//        }
+//
+//        if (a<b){
+//            System.out.println("<true>");
+//        }
+//
+//        if (a>=b){
+//            System.out.println(">=");
+//        }
+//
+//        if (a<=b){
+//            System.out.println("<=");
+//        }
+//        int a=1,b;
+//
+//        b=++a;
+//        System.out.println(b);
+
+        int a=3,b=2;
+
+        if (a>b || a>0){
+            System.out.println("true");
+        }
+        if (a>b && a>0){
+            System.out.println("true");
+        }
+
+    }
+}
